209f17c03817fe2f825e4df34e8ed54f17229e49
Josh <Josh@10.0.0.12>
2016-10-20 23:54:22 -0400

    Trying to push changes to testing...

--- src/release1/ReleaseOneTesting.java
+++ src/release1/ReleaseOneTesting.java
@@ -45,7 +45,7 @@ public class ReleaseOneTesting {
     assertEquals("long", test.getHair());
   }
   
-  //
+  //code ninja
   @Test
   public void testGetEyes() {
     Character test = new Character("test", "long", "green", "red");
