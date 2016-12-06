eb52a65ca5b2edd064ae7f0257b575f3e2835951
Stacy <Stacy@35.40.118.207>
2016-12-06 18:49:29 -0500

    Testing for maze

---
+++ src/release1/MazeEngineTesting.java
@@ -0,0 +1,14 @@
+package release1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MazeEngineTesting {
+  
+  public void constructerTest() {
+   MazeEngine test = new MazeEngine(5);
+   
+  }
+ 
+}
