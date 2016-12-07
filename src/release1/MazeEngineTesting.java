2e57f6e0afa9b7c297dddc5a571141b6f6d66558
Admin <Admin@Admin-PC.att.net>
2016-12-06 22:28:15 -0500

    Checkstyle for MazeEngineTesting

--- src/release1/MazeEngineTesting.java
+++ src/release1/MazeEngineTesting.java
@@ -1,13 +1,26 @@
 package release1;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-import org.junit.Test;
+//import org.junit.Test;
 
+/**
+ * 
+ * @author Patton Finley, Josh Crum, Paul Magee
+ *
+ */
 public class MazeEngineTesting {
   
-  public void constructerTest() {
-   MazeEngine test = new MazeEngine(5);
+	/**
+	 * Magic Number.
+	 */
+	  public static final int FIVE = 5;
+	
+	/**
+	 * Constructor Test.
+	 */
+  public final void constructerTest() {
+   MazeEngine test = new MazeEngine(FIVE);
    
   }
  
